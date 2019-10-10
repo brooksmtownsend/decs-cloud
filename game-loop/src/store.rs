07370be1b3854822141bb9b898d24c56376d8314
Kevin Hoffman <alothien@gmail.com>
2019-10-10 10:18:21 -0400

    Fixing bug in game loop's access of the shard list

--- game-loop/src/store.rs
+++ game-loop/src/store.rs
@@ -1,8 +1,12 @@
 use guest::prelude::*;
 
-const SHARDS_KEY: &str = "shards";
-const MAX_SHARDS: isize = 1_000;
+const SHARDS_KEY: &str = "decs:shards";
 
+
+// TODO: game loop should not be tightly coupled to the shard schema. It should
+// instead make a request of the shard manager to get a list of shards. We should
+// evaluate this once we have benchmarking capabilities to see how it might impact
+// throughput.
 pub(crate) fn get_shards(ctx: &CapabilitiesContext) -> Result<Vec<String>> {
-    ctx.kv().list_range(SHARDS_KEY, 0, MAX_SHARDS)
+    ctx.kv().set_members(SHARDS_KEY)
 }
